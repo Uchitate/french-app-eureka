65b959e9c4d73b70af1c1f479b77b74eb0eee75a
内立良介 <uchitate@tagbangers.co.jp>
2016-10-19 21:22:13 +0900

    最適化

--- src/main/java/co/jp/tagbangers/Application.java
+++ src/main/java/co/jp/tagbangers/Application.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class FrenchAppApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(FrenchAppApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 }
